72638381c23001ad23e369f2e6c5a536186eda1f
danielmezo <daniel.mezo@balmes.epiaedu.cat>
2018-10-02 19:04:28 +0200

    feat: Definicion de los metodos de lista.h y empezamos a programar los metodos de lista.c

--- lista.c
+++ lista.c
@@ -3,3 +3,65 @@
 //
 
 #include "lista.h"
+
+listaBID listabidCreate(){
+    Lista l;
+
+    l.first = (Node *) malloc (sizeof(Node)); //Asignacion de memoria
+    if(l.first == NULL)
+    {
+        printf("Error al crear la llista\n");
+    }else{
+        l.last=(Node *) malloc (sizeof(Node));
+        if(l.last==NULL)
+        {
+            printf("Error al crear la llista\n");
+            free(l.first);
+        }else{
+            l.pdi=l.last;
+            l.first->seguent=l.last;
+            l.last->anterior=l.first;
+            l.first->anterior=NULL;
+            l.last->seguent=NULL;
+        }
+    }
+    return l;
+}
+void listabidIntroduceDelante(listaBID *lista, elemento e)
+{
+    Node *aux;
+    aux = (Node *) malloc (sizeof(Node));
+    if(aux==NULL)
+    {
+        printf("Error al insertar\n");
+    }else{
+        aux->seguent=l->pdi;
+        aux->e=e;
+        aux->anterior=l.>pdi->anterior;
+        aux->anterior->seguent=aux;
+        l->pdi->anterior= aux;
+    }
+}
+void listabidIntroduceDetras(listaBID *lista, elemento e){
+
+}
+element listabidConsulta(listaBID lista);
+void listabidBorrar(listaBID *lista){
+    Node *aux;
+    if(l->pdi==l->first || l->pdi == l->last)
+    {
+        printf("Error al insertar\n");
+    }else{
+        aux=l->pdi;
+        l->pdi=l->pdi->seguent;
+        aux->anterior->seguent=aux->seguent;
+        free(aux);
+    }
+}
+void listabidAvanza(listaBID *lista){}
+void listabidRetrocede(listaBID *lista){}
+int listabidFinal(listaBID lista){}
+int listabidInicio(listaBID lista){}
+void llistabidGoFirst(listaBID *lista){}
+int listabidBacia(listaBID lista){}
+void listabidDestruye(listaBID *lista){}
--- lista.h
+++ lista.h
@@ -4,5 +4,28 @@
 
 #ifndef EJERCICIO1_LISTA_H
 #define EJERCICIO1_LISTA_H
+typedef struct n{
+    element e;
+    struct n *anterior;
+    struct n *seguent;
+}Node;
+typedef struct{
+    Node *first;
+    Node *last;
+    Node *pdi;
+}listaBID;
+
+listaBID listabidCreate();
+void listabidIntroduceDelante(listaBID *lista, elemento e);
+void listabidIntroduceDetras(listaBID *lista, elemento e);
+element listabidConsulta(listaBID lista);
+void listabidBorrar(listaBID *lista);
+void listabidAvanza(listaBID *lista);//Tira una posicion hacia el final de la cola
+void listabidRetrocede(listaBID *lista);//Tira una posicion hacia adelante de la cola
+int listabidFinal(listaBID lista);
+int listabidInicio(listaBID lista);
+void llistabidGoFirst(listaBID *lista);
+int listabidBacia(listaBID lista);
+void listabidDestruye(listaBID *lista);
 
 #endif //EJERCICIO1_LISTA_H
